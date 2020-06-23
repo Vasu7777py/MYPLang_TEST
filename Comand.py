b42e374e91417051f9dd228de02a390a6ecf7660
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-23 20:31:23 +0530

    added lexer and position

--- Comand.py
+++ Comand.py
@@ -2,6 +2,13 @@
 from dataclasses import dataclass
 
 # #######################################################################
+# GLOBAL
+# #######################################################################
+
+RUN_FILE = None
+FILE_LOCATION = ""
+
+# #######################################################################
 # CONSTANTS
 # #######################################################################
 
@@ -18,6 +25,62 @@ class Token:
 
     def __repr__(self):
         return f"\"_type_ : {self._type_}, value : {self.value}\""
+
+# #######################################################################
+# POSITION
+# #######################################################################
+
+@dataclass
+class Position:
+    def __init__(self, index, column_number, line_number, file_name):
+        self.index = index
+        self.column_number = column_number
+        self.line_number = line_number
+        self.file_name = file_name
+    
+    def copy(self):
+        return Position(self.index, self.column_number, self.line_number, self.file_name)
+
+    def __repr__(self):
+        return f"index : {self.index}, column_number : {self.column_number}, line_number : {self.line_number}, file_name : {self.file_name}"
+
+    def advance(self, current_char):
+        self.index += 1
+        self.column_number += 1
+
+        if (current_char == "\n"):
+            self.column_number = 0
+            self.line_number += 1
+
+# #######################################################################
+# LEXER
+# #######################################################################
+
+@dataclass
+class Lexer:
+    def __init__(self, file_name, file):
+        self.file = file
+        self.lines = self.file.readlines()
+        if (len(self.lines) < 1):
+            exit()
+        self.file_name = file_name
+        self.position = Position(0, 0, 0, file_name)
+        self.tokens = None
+        self.current_line = self.lines[self.position.line_number]
+        self.current_char = self.current_line[0]
+    
+    def advance(self):
+        self.position.advance(self.current_char)
+        if (self.current_char == "\n"):
+            if (self.position.line_number < len(self.lines)):
+                self.current_line = self.lines[self.position.line_number]
+            else:
+                self.current_char = None
+                return
+        self.current_char = self.current_line[self.position.column_number]
+
+    def create_tokens(self):
+        pass
 
 # #######################################################################
 # KEYWORDS
@@ -38,4 +101,11 @@ KEYWORDS = {
     "NULL" : {},
 }
 
-print(KEYWORDS, "\n", KEYWORDS.keys())
+# #######################################################################
+# RUN
+# #######################################################################
+
+def run(file_name, file_location):
+    RUN_FILE = open(f"{file_location}/{file_name}.txt", "r")
+    print(RUN_FILE.read())
+    RUN_FILE.close()
---
+++ Shell.py
@@ -0,0 +1,6 @@
+import Comand
+
+file_name = input("Enter the file_name : ")
+file_location = input("Enter file_location : ")
+
+Comand.run(file_name, file_location)
